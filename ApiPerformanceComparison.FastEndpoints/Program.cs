4f6f54a864ecec33329e633839d8317b55aa9012
Gabegi <gabriel_pirastru@hotmail.fr>
2025-09-09 17:45:44 +0800

    fastendpoints benchmarks

--- ApiPerformanceComparison.FastEndpoints/Program.cs
+++ ApiPerformanceComparison.FastEndpoints/Program.cs
@@ -1,6 +1,5 @@
 using ApiPerformanceComparison.Shared;
 using FastEndpoints;
-using FastEndpoints.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +8,6 @@ var products = QuickSeeder.SeedProducts(100_000);
 builder.Services.AddSingleton(products);
 
 builder.Services.AddFastEndpoints();
-builder.Services.SwaggerDocument(o =>
-{
-    o.EnableJWTBearer = false;
-    o.DocumentSettings = s => s.Title = "API Performance Comparison - FastEndpoints";
-});
 
 var app = builder.Build();
 
@@ -23,7 +17,6 @@ if (!app.Environment.IsEnvironment("Testing"))
 }
 
 app.UseFastEndpoints();
-app.UseSwaggerGen();
 
 app.Run();
 
