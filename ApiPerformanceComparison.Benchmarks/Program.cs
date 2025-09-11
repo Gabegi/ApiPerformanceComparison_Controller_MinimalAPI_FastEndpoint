89a76529890078044446c6c0bd96ad805d30a570
Gabegi <gabriel_pirastru@hotmail.fr>
2025-09-11 18:03:52 +0545

    new benchmarks

--- ApiPerformanceComparison.Benchmarks/Program.cs
+++ ApiPerformanceComparison.Benchmarks/Program.cs
@@ -1,4 +1,8 @@
 ﻿using ApiPerformanceComparison.Benchmarks;
 using BenchmarkDotNet.Running;
-    
+
 BenchmarkRunner.Run<ProductsApiBenchmark>();
+
+BenchmarkRunner.Run<ProductsControllerBenchmark>();
+BenchmarkRunner.Run<ProductsMinimalApiBenchmark>();
+BenchmarkRunner.Run<ProductsFastEndpointsBenchmark>();
