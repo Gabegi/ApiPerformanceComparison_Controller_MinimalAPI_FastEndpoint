a0ddc2af2d45d0777e9a16854846b955d7dba0cc
Gabegi <gabriel_pirastru@hotmail.fr>
2025-09-17 08:58:00 +0700

    new controller

--- ApiPerformanceComparison.Shared/AtomicCounter.cs
+++ ApiPerformanceComparison.Shared/AtomicCounter.cs
@@ -3,6 +3,18 @@ using System;
 namespace ApiPerformanceComparison.Shared;
 
 public class AtomicCounter
-{
-    
-}
+    {
+        private int _value;
+
+        public AtomicCounter(int initialValue = 0)
+        {
+            _value = initialValue;
+        }
+
+        public int GetNext()
+        {
+            return Interlocked.Increment(ref _value);
+        }
+
+        public int Current => _value;
+    }
